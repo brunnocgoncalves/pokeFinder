5bb1a921376eea7f7b2fe1cfac5b39a40873f836
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-11-26 11:58:32 -0200

    Created functions and delegates for LocationManager and center the map

--- pokeFinder/ViewController.swift
+++ pokeFinder/ViewController.swift
@@ -7,16 +7,101 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     var geoFire: GeoFire!
+    var geoFireReference: FIRDatabaseReference!
+    var mapIsCentered: Bool = false
     
     @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mapView.delegate = self
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+        
+        locationManager.delegate = self
+        
+        geoFireReference = FIRDatabase.database().reference()
+        geoFire = GeoFire(firebaseRef: geoFireReference)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        locationAuthStatus()
+        
+    }
+    
+    func locationAuthStatus(){
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            
+            mapView.showsUserLocation = true
+            
+        } else{
+            
+            locationManager.requestWhenInUseAuthorization()
+            
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            
+            mapView.showsUserLocation = true
+            
+        }
+        
+    }
+    
+    func centerMapCurrentLocation(location: CLLocation){
+        
+        let coordinate = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinate, animated: true)
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let location = userLocation.location{
+            
+            if !mapIsCentered{
+                
+                centerMapCurrentLocation(location: location)
+                mapIsCentered = true
+                
+            }
+            
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView: MKAnnotationView?
+        
+        if annotation.isKind(of: MKUserLocation.self){
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView?.image = UIImage(named: "ash")
+            
+        }
+        
+        return annotationView
+        
+    }
+    
+    @IBAction func pokeballPressed(_ sender: AnyObject){
         
         
         
