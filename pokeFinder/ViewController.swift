2edea23c8e6fe096bac0fe425dd187f9514827e4
Brunno Goncalves <brunnocgoncalves@gmail.com>
2016-11-26 18:12:56 -0200

    Finished adding map markers and directions

--- pokeFinder/ViewController.swift
+++ pokeFinder/ViewController.swift
@@ -89,11 +89,34 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         
         var annotationView: MKAnnotationView?
+        let annotationId: String = "Pokemon"
         
         if annotation.isKind(of: MKUserLocation.self){
             
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
             annotationView?.image = UIImage(named: "ash")
+            
+        } else if let dequeAnno = mapView.dequeueReusableAnnotationView(withIdentifier: annotationId){
+            
+            annotationView = dequeAnno
+            annotationView?.annotation = annotation
+            
+        } else{
+            
+            let annoView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationId)
+            annoView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView = annoView
+            
+        }
+        
+        if let annotationView = annotationView, let annotation = annotation as? PokeAnnotation{
+            
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "\(annotation.podeId)")
+            let button = UIButton()
+            button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            button.setImage(UIImage(named: "map"), for: .normal)
+            annotationView.rightCalloutAccessoryView = button
             
         }
         
@@ -104,6 +127,29 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     func createPick(forLocation location: CLLocation, withPokemon pokeId: Int){
         
         geoFire.setLocation(location, forKey: "\(pokeId)")
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        
+        let location = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        showPokeOnMap(location: location)
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if let annotation = view.annotation as? PokeAnnotation{
+            
+            let placeMark = MKPlacemark(coordinate: annotation.coordinate)
+            let destination = MKMapItem(placemark: placeMark)
+            destination.name = "Pokemon found"
+            let regionDist: CLLocationDistance = 1000
+            let regionSpan = MKCoordinateRegionMakeWithDistance(annotation.coordinate, regionDist, regionDist)
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving] as [String : Any]
+            MKMapItem.openMaps(with: [destination], launchOptions: options)
+            
+        }
         
     }
     
@@ -127,7 +173,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     @IBAction func pokeballPressed(_ sender: AnyObject){
         
         let location = CLLocation(latitude: mapView.centerCoordinate.latitude, longitude: mapView.centerCoordinate.longitude)
+        let rand = arc4random_uniform(151) + 1
         
+        createPick(forLocation: location, withPokemon: Int(rand))
         
     }
 
